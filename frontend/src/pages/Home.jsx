b76668b870d74f24432dd831ccff14f2ae916fcb
OnlineEye Technologies <146831834+Onlineeyetech@users.noreply.github.com>
2026-05-24 14:54:54 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -21,7 +21,6 @@ export default function Home() {
       <Services />
       <WhyChooseUs />
       <Portfolio />
-        <VideoShowcase />
       <Process />
       <Testimonials />
       <Founder />
